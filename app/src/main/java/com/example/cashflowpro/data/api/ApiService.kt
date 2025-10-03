1a16fb5ab8ebec7627d7e1871c1ce6be0b5b0ba7
vedantdalvi45 <vedalvi16@gmail.com>
2025-10-03 15:33:14 +0530

    Added : Categories Endpoint to ApiService.kt

--- app/src/main/java/com/example/cashflowpro/data/api/ApiService.kt
+++ app/src/main/java/com/example/cashflowpro/data/api/ApiService.kt
@@ -1,11 +1,14 @@
 package com.example.cashflowpro.data.api
 
+import com.example.cashflowpro.data.model.Category
 import com.example.cashflowpro.data.model.auth.GenericResponse
 import com.example.cashflowpro.data.model.auth.LoginRequest
 import com.example.cashflowpro.data.model.auth.LoginResponse
 import com.example.cashflowpro.data.model.auth.SignUpRequest
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
@@ -15,5 +18,10 @@ interface ApiService {
 
     @POST("auth/signup")
     suspend fun signupUser(@Body signUpRequest: SignUpRequest): Response<GenericResponse>
+
+    @GET("categories/all")
+    suspend fun getAllCategories(
+        @Header("Authorization") token: String
+    ): Response<List<Category>>
 
 }
