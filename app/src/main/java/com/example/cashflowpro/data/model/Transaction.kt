325a86a8e9b78a780f4e4c37fe330659e6f3791b
vedantdalvi45 <vedalvi16@gmail.com>
2025-10-05 02:04:13 +0530

    Fix : Change Response of Transaction Endpoint in Transaction.kt

--- app/src/main/java/com/example/cashflowpro/data/model/Transaction.kt
+++ app/src/main/java/com/example/cashflowpro/data/model/Transaction.kt
@@ -1,17 +1,19 @@
 package com.example.cashflowpro.data.model
 
+import java.math.BigDecimal
+import java.time.Instant
 import java.util.Date
 
 data class Transaction(
-    val id: Long,
-    val amount: String,
-    val description: String,
-    val timestamp: Long = System.currentTimeMillis(),
-    val date: Date = Date(timestamp),
-    val category: Category,
-    val paymentMethod: PaymentMode,
-    val balance: Double,
-    val transactionType: String,
-    val tags: List<String>,
-    val attachment : String
+    val id: Long? = null,
+    val type: String,
+    val amount: BigDecimal,
+    val description: String?,
+    var time: Date,
+
+    // IDs of related entities
+    val categoryId: Long?,
+    val fromPaymentModeId: Long? = null,
+    val toPaymentModeId: Long?,
+    val tagIds: Set<Long>? = null
 )
